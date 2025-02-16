60fc2f00109b913b544ad3f176febd5104f5c077
Rohan859 <rohankundu859@gmail.com>
2025-02-16 22:57:48 +0530

    added the local storage and issue fixed when we are trying refresh the page after the refresh, data will be fetched from localstorage if already available

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
 let inputEl = document.getElementById('input-el');
 let buttonEl = document.getElementById('input-btn');
 let displayEl = document.getElementById('display-el')
-displayEl.style.display = 'none';
 let inputArr = [];
+let localStorageArr = JSON.parse(localStorage.getItem('input-ele'));
 
 
+if(localStorageArr) 
+{
+    inputArr = localStorageArr;
+    console.log(inputArr);
+    render();
+}
 
 buttonEl.addEventListener('click', () => {
-    displayEl.style.display = 'block';
     inputArr.push(inputEl.value);
-    console.log(inputArr);
     localStorage.setItem('input-ele', JSON.stringify(inputArr));
     inputEl.value = '';
     render();
@@ -21,16 +25,17 @@ buttonEl.removeEventListener('click', () => {}
 
 
 function render() {
-    let localStorageArr = JSON.parse(localStorage.getItem('input-ele'));
+
     displayEl.innerHTML = ''; // Clear previous list items
 
-    for (let key in localStorageArr) {
+    for (let key in inputArr) {
         let li = document.createElement('li');
         let a = document.createElement('a');
-        a.href = localStorageArr[key].startsWith('https') ? localStorageArr[key] : `https://${localStorageArr[key]}`;
-        a.textContent = localStorageArr[key];
+        a.href = inputArr[key].startsWith('https') ? inputArr[key] : `https://${inputArr[key]}`;
+        a.textContent = inputArr[key];
         a.target = '_blank';
         li.appendChild(a);
         displayEl.appendChild(li);
     }
 }
+
