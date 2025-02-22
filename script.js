ad29a41456c4edef485d028dbf11436cee56a27a
Rohan859 <rohankundu859@gmail.com>
2025-02-22 16:13:10 +0530

    Removed the LocalStorage and connected Firebase database
    1. Removed the LocalStorage
    2. connected Firebase database
    3. Styles added for label
    4. Icon added in the title

--- script.js
+++ script.js
@@ -1,40 +1,80 @@
-let inputEl = document.getElementById('input-el');
-let buttonEl = document.getElementById('input-btn');
-let displayEl = document.getElementById('display-el')
-let deleteBtn = document.getElementById('delete-btn');
-let saveTabBtn = document.getElementById('save-tab-btn');
-let inputArr = [];
-
-let localStorageArr = JSON.parse(localStorage.getItem('input-ele'));
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.3.1/firebase-app.js";
+import { getDatabase, 
+         ref,
+         push, 
+         onValue,
+         remove} from "https://www.gstatic.com/firebasejs/11.3.1/firebase-database.js";
 
 
-if(localStorageArr) 
+
+
+const firebaseConfig = 
 {
-    inputArr = localStorageArr;
-    render();
+    databaseURL: 'https://chrome-extension-a160b-default-rtdb.asia-southeast1.firebasedatabase.app/',
+    apiKey: "AIzaSyAnHNd5dAapXk5uCRihBbazudwuHdFF1E8",
+    authDomain: "chrome-extension-a160b.firebaseapp.com",
+    projectId: "chrome-extension-a160b",
+    storageBucket: "chrome-extension-a160b.firebasestorage.app",
+    messagingSenderId: "781378670114",
+    appId: "1:781378670114:web:f4ebc56fc4b44d1ddfc47b"
 }
 
-buttonEl.addEventListener('click', () => {
-    inputArr.push(inputEl.value);
-    localStorage.setItem('input-ele', JSON.stringify(inputArr));
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+const dbRef = ref(database,"Rohan'sDatabase");
+
+
+console.log(database);
+console.log(firebaseConfig.databaseUrl);
+console.log(dbRef);
+
+
+let inputEl = document.getElementById('input-el');
+let saveBtn = document.getElementById('input-btn');
+let displayEl = document.getElementById('display-el')
+let deleteBtn = document.getElementById('delete-btn');
+
+
+
+
+saveBtn.addEventListener('click', () => {
+    push(dbRef,inputEl.value);
     inputEl.value = '';
-    render();
 });
 
 
-buttonEl.removeEventListener('click', () => {}
-);
+deleteBtn.addEventListener('dblclick', () => {
+    remove(dbRef);
+    displayEl.innerHTML = '';
+});
 
 
-function render() {
+
+
+onValue(dbRef, (snapshot) => {
+    if(snapshot.exists()) 
+    {
+        const data = snapshot.val();
+        if(data != null)
+        {
+            const leadsArr = Object.values(data);
+            console.log(leadsArr);
+            render(leadsArr);
+        }
+    }
+});
+
+
+
+function render(leads) {
 
     displayEl.innerHTML = ''; // Clear previous list items
 
-    for (let key in inputArr) {
+    for (let key in leads) {
         let li = document.createElement('li');
         let a = document.createElement('a');
-        a.href = inputArr[key].startsWith('https') ? inputArr[key] : `https://${inputArr[key]}`;
-        a.textContent = inputArr[key];
+        a.href = leads[key].startsWith('https') ? leads[key] : `https://${leads[key]}`;
+        a.textContent = leads[key];
         a.target = '_blank';
         li.appendChild(a);
         displayEl.appendChild(li);
@@ -42,19 +82,4 @@ function render() {
 }
 
 
-deleteBtn.addEventListener('click', () => {
-    localStorage.clear();
-    inputArr = [];
-    render();
-});
-
-
-saveTabBtn.addEventListener('click', () => {
-    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-        let activeTab = tabs[0];
-        inputArr.push(activeTab.url);
-        localStorage.setItem('input-ele', JSON.stringify(inputArr));
-        render();
-    });
-});
 
